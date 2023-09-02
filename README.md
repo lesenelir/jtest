872b947add660808313ac99ecac136e085ee3b18
lesenelir <hi@lesenelir.me>
2023-09-02 17:42:21 +0800

    feat: project add webpack + react

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # JTest
 
 jest tutorial - some test examples written with Jest
+
+### Test Driven Development ( TDD ) 
+Test Falls -> Test Passes -> Refactor -> Test Falls ...
+
+> TDD mode is wildly used in utility functions
+> 
+> Test cases is always the use cases. You can also look at the test cases ( use cases ) as a documentation for the function ~ 
+  -  pure function scenarios ( Easy to think of input and output )
+  -  BUG fixing scenarios ( when encountering a bug, write a test case to reproduce the bug, then fix the bug )
+  -  UI interaction scenarios ( use test to simulate user interaction, then implement the main logic )
+
+### Behavior Driven Development (BDD)
+
+First, develop the main logic, then write the test cases to test the important part.
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('node:path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.tsx'
+  },
+  module: {
+    rules: [
+      // parse ts
+      {
+        test: /\.(jsx|tsx)$/,
+        use: 'ts-loader',
+        exclude: /(node_modules|tests)/
+      },
+      // parse css
+      {
+        test: /\.css$/i,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', 'css'],
+    // alias:
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 6324,
+    hot: true
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
